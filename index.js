81098a26e5921ba7c051971885a82d400deee334
Carrie <carrie.murchi@gmail.com>
2024-10-25 17:27:24 -0700

    'nav bar updqtes'

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const React = require("react");
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../server/src/App';
-import reportWebVitals from '../server/src/reportWebVitals';
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
 
 
