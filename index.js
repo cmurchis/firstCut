22e2637bf2a99e2e1dd6557d79eb57e9c5b315b2
Carrie <carrie.murchi@gmail.com>
2024-10-27 14:19:36 -0700

    'fix sticky buttons and image overlays'

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mullet = document.getElementById('mullet');
 const scissors = document.getElementById('scissors');
-const scheduling
 
 const toggleQ = (x, y) => {
   if(x.style.display == 'block'){
@@ -17,11 +16,11 @@ setInterval(toggleQ, 3000, mullet, scissors)
 
 window.addEventListener('scroll', function() {
   
-  const book = document.getElementById('book')
+  const book = document.getElementById('navi')
   if (window.scrollY > 10) { // Adjust this threshold as needed
-    book.classList.add('sticky');
+    navi.classList.add('sticky');
   } else {
-    book.classList.remove('sticky');
+    navi.classList.remove('sticky');
   }
 });
 
