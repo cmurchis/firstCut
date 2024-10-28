04defca8019625c68bfe70edca69a62750693ea7
Carrie <carrie.murchi@gmail.com>
2024-10-28 03:55:05 -0700

    'add chair div'

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const mullet = document.getElementById('mullet');
 const scissors = document.getElementById('scissors');
 
 const toggleQ = (x, y) => {
-  if(x.style.display == 'block'){
+  if(x.style.display == 'inline'){
   x.style.display = 'none';
-  y.style.display = 'block';
+  y.style.display = 'inline';
   }else{
-    x.style.display = 'block';
+    x.style.display = 'inline';
   y.style.display = 'none';
   }
 
