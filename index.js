ed9cbf52ff00533a2ddd691f8fde37530a0ec7c4
Carrie <carrie.murchi@gmail.com>
2024-10-31 14:21:10 -0700

    'css animations, more imgs, js functionality'

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 const mullet = document.getElementById('mullet');
 const scissors = document.getElementById('scissors');
 
-const toggleQ = (x, y) => {
-  if(x.style.display == 'inline'){
-  x.style.display = 'none';
-  y.style.display = 'inline';
+const toggle = (x, y) => {
+  if(x.hidden){
+  x.hidden = false;
+  y.hidden = true;
   }else{
-    x.style.display = 'inline';
-  y.style.display = 'none';
+    x.setAttribute('hidden', false) ;
+    y.setAttribute('hidden', true);
   }
 
 };
-setInterval(toggleQ, 3000, mullet, scissors)
+
+setInterval(toggle, 3000, mullet, scissors);
 
 
-window.addEventListener('scroll', function() {
-  
-  const book = document.getElementById('navi')
-  if (window.scrollY > 10) { // Adjust this threshold as needed
-    navi.classList.add('sticky');
-  } else {
-    navi.classList.remove('sticky');
-  }
-});
+const evenE = document.getElementById('even')
+const oddE = document.getElementById('odd')
+const evenImg = document.getElementById('evenImg');
+const oddImg = document.getElementById('oddImg');
+const gifts = document.getElementById('gifts')
 
-var pos = 0;
-let num = 1;
-function move(x, y) {
- while(y < 4){
-  let kid = document.getElementById(`kid`);
-  kid.setAttribute('src', `img/boi${y}`);
-  kid.setAttribute('background-image', `img/boi${y}`)
-  kid.hidden = false
-  if(x == 40) {
-    kid.classList.remove("tumble");
-    kid.hidden = true;
-    y++
-    kid.removeAttribute('src')
-} else {
-  x++
-  kid.classList.add('tumble')  
-  kid.style.paddingLeft = x + '%;';
-    }
-
+const toggleElement = () => {
+  if(gifts.hidden == true){
+    gifts.hidden = false;
+  }else{
+    gifts.hidden = true;
   }
 }
 
-setInterval(move(), 100, pos, num)
+
+function showHide() {
+  if(gifts.hidden){
+    gifts.hidden = false;
+  }else{
+    gifts.setAttribute('hidden', 'true')  
+  }
+}
+
+function animate(){
+  if(oddE.hidden == true){
+    oddE.hidden = false;
+    evenE.hidden = true;
+   
+  }else{
+    evenE.hidden = false;
+    oddE.hidden = true;
+   
+  }
+}
+
+setInterval(animate, 5000)
